38517b30a2c6b3c74b422cb44f5de3cbd12606cb
Kishore Chandrasekharan <kishore28m@gmail.com>
2019-05-26 01:30:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,15 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element){
+  var cities = array;
+  cities = [...cities,element];
+  return cities;
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  var len = array.length;
+  array[len] = element;
+  return array;
+}
