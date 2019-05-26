96af0de0f0e9ec2fc087fc98368840838c8b548a
Kishore Chandrasekharan <kishore28m@gmail.com>
2019-05-26 01:45:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,9 +28,11 @@ function accessElementInArray(array,index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift();
+  array.shift();
+  return array;
 }
 
 function removeElementFromBeginningOfArray(array){
-  return array.slice(1);
+  array.slice(1);
+  return array;
 }
